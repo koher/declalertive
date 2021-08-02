81c8672a989ccfa136adee8590dd31f8b604233f
Yuta Koshizawa <koher@koherent.org>
2021-08-03 08:41:56 +0900

    Add Sample

---
+++ Sample/Sample/ViewController.swift
@@ -0,0 +1,52 @@
+import UIKit
+import Combine
+import Declalertive
+
+class ViewController: UIViewController {
+    final class State: ObservableObject {
+        @Published var presentsAlert: Bool = false
+    }
+
+    private let state: State = .init()
+    private var cancellables: Set<AnyCancellable> = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Makes a `UIAlertController` instance
+        let alertController: UIAlertController = .init(title: "Title", message: "Message", preferredStyle: .alert)
+        alertController.addAction(.init(title: "Cancel", style: .cancel) { [weak self] _ in
+            print("Cancel")
+            self?.state.presentsAlert = false
+        })
+        alertController.addAction(.init(title: "OK", style: .default) { [weak self] _ in
+            print("OK")
+            self?.state.presentsAlert = false
+        })
+
+        state
+            .objectWillChange
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] _ in
+                guard let self = self else { return }
+                // Control the `UIAlertController`'s presentations
+                self.setPresents(self.state.presentsAlert, alertController: alertController, animated: true)
+            }
+            .store(in: &cancellables)
+        
+        // Layout
+        let button: UIButton = .init()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Alert", for: .normal)
+        button.setTitleColor(view.tintColor, for: .normal)
+        button.addAction(.init { [weak self] _ in
+            guard let self = self else { return }
+            self.state.presentsAlert = true
+        }, for: .touchUpInside)
+        view.addSubview(button)
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        ])
+    }
+}
